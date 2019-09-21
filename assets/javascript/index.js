1c4d66149f41721cc8579b674e48866b1564a18f
Oliver Sun <olisun@mac.com>
2019-09-21 13:26:15 -0700

    added links to github repos and deployed sites for projects. started experimenting with modals

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -21,58 +21,6 @@ $(document).ready(function() {
 
   $('#accordion').accordion();
 
-  $('#gify-img').on('mouseover', function() {
-    $('#hideGify').css('display', 'flex');
-    $('#train-img').css('display', 'none');
-    $('#hideTrain').css('display', 'none');
-    $('#trivia-img').css('display', 'none');
-    $('#hideTrivia').css('display', 'none');
-  })
-
-  $('#gify-img').on('mouseout', function() {
-    $('#hideGify').css('display', 'none');
-    $('#train-img').css('display', 'flex');
-    $('#hideTrain').css('display', 'none');
-    $('#trivia-img').css('display', 'flex');
-    $('#hideTrivia').css('display', 'none');
-  })
-
-  $('#train-img').on('mouseover', function() {
-    $('#hideTrain').css('display', 'flex');
-    $('#gify-img').css('display', 'none');
-    $('#hideGify').css('display', 'none');
-    $('#trivia-img').css('display', 'none');
-    $('#hideTrivia').css('display', 'none');
-  })
-
-  $('#train-img').on('mouseout', function() {
-    $('#hideTrain').css('display', 'none');
-    $('#gify-img').css('display', 'flex');
-    $('#hideGify').css('display', 'none');
-    $('#trivia-img').css('display', 'flex');
-    $('#hideTrivia').css('display', 'none');
-  })
-
-
-  $('#trivia-img').on('mouseover', function() {
-    $('#hideTrivia').css('display', 'flex');
-    $('#gify-img').css('display', 'none');
-    $('#hideGify').css('display', 'none');
-    $('#train-img').css('display', 'none');
-    $('#hideTrain').css('display', 'none');
-
-  })
-
-  $('#trivia-img').on('mouseout', function() {
-    $('#hideTrivia').css('display', 'none');
-    $('#gify-img').css('display', 'flex');
-    $('#hideGify').css('display', 'none');
-    $('#train-img').css('display', 'flex');
-    $('#hideTrain').css('display', 'none');
-  })
-
-
-
 
 
 });
