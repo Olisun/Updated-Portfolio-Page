443167075206ad7789529ec7191d1b1b8f057840
Oliver Sun <olisun@mac.com>
2019-09-22 09:31:18 -0700

    added responsiveness css. updated animation on load js. added group project to portfolio section html

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -4,12 +4,29 @@ $(document).ready(function() {
   // Adding background color to the accordian header. 
   $('.ui-accordion-header').css('background', 'rgba(32, 32, 172, 1)');
 
-  // $('#test').on('click', function() {
-  $('.display-10').animate({ fontSize: '12px', color: 'rgba(206, 25, 25, 1);', opacity: '0.4' }, 'slow');
-  $('.display-10').animate({ fontSize: '18px', color: 'rgba(32, 32, 172, 1);', opacity: '0.7' }, 'slow');
-  $('.display-10').animate({ fontSize: '24px', color: 'rgba(255, 255, 255, 1);', opacity: '1' }, 'slow');
+  setTimeout(function() {
+    $('.display-10').animate({ fontSize: '0px', color: 'rgba(255, 255, 255, 1);', opacity: '0.0' }, 'fast');
+    $('.display-10').animate({ fontSize: '2px', color: 'rgba(255, 255, 255, 1);', opacity: '0.1' }, 'fast');
+    $('.display-10').animate({ fontSize: '4px', color: 'rgba(206, 25, 25, 1);', opacity: '0.2' }, 'fast');
+    $('.display-10').animate({ fontSize: '6px', color: 'rgba(206, 25, 25, 1);', opacity: '0.3' }, 'fast');
+    $('.display-10').animate({ fontSize: '8px', color: 'rgba(255, 255, 255, 1);', opacity: '0.4' }, 'fast');
+    $('.display-10').animate({ fontSize: '10px', color: 'rgba(255, 255, 255, 1);', opacity: '0.5' }, 'fast');
+    $('.display-10').animate({ fontSize: '12px', color: 'rgba(32, 32, 172, 1);', opacity: '0.6' }, 'fast');
 
-  // })
+    $('.display-10').animate({ fontSize: '14px', color: 'rgba(32, 32, 172, 1);', opacity: '0.7' }, 'fast');
+    $('.display-10').animate({ fontSize: '16px', color: 'rgba(255, 255, 255, 1);', opacity: '0.8' }, 'fast');
+    $('.display-10').animate({ fontSize: '18px', color: 'rgba(255, 255, 255, 1);', opacity: '0.9' }, 'fast');
+    $('.display-10').animate({ fontSize: '20px', color: 'rgba(206, 25, 25, 1);', opacity: '1' }, 'fast');
+    $('.display-10').animate({ fontSize: '21px', color: 'rgba(206, 25, 25, 1);', opacity: '1' }, 'fast');
+    $('.display-10').animate({ fontSize: '22px', color: 'rgba(255, 255, 255, 1);', opacity: '1' }, 'fast');
+    $('.display-10').animate({ fontSize: '23px', color: 'rgba(255, 255, 255, 1);', opacity: '1' }, 'fast');
+    $('.display-10').animate({ fontSize: '24px', color: 'rgba(255, 255, 255, 1);', opacity: '1' }, 'fast');
+    $('.display-10').animate({ color: 'rgba(206, 25, 25, 1);', opacity: '1' }, 'slow');
+    $('.display-10').animate({ color: 'rgba(32, 32, 172, 1);', opacity: '1' }, 'slow');
+    $('.display-10').animate({ color: 'rgba(255, 255, 255, 1);', opacity: '1' }, 'slow');
+
+  }, 1000);
+
 
   // setInterval(function() {
   //   $('.display-10').delay(1000).animate({ 'fontSize': '12px' }), 1000, 'swing',
