ff319e4da1737a1240f652678abb67f0cff55cf4
Oliver Sun <olisun@mac.com>
2019-09-21 23:27:05 -0700

    added some animation to jumbotron in js

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+  // This calls on jQuery's built-in accordian method and activates the accordian's actions.
+  $('#accordion').accordion();
+  // Adding background color to the accordian header. 
+  $('.ui-accordion-header').css('background', 'rgba(32, 32, 172, 1)');
+
+  // $('#test').on('click', function() {
+  $('.display-10').animate({ fontSize: '12px', color: 'rgba(206, 25, 25, 1);', opacity: '0.4' }, 'slow');
+  $('.display-10').animate({ fontSize: '18px', color: 'rgba(32, 32, 172, 1);', opacity: '0.7' }, 'slow');
+  $('.display-10').animate({ fontSize: '24px', color: 'rgba(255, 255, 255, 1);', opacity: '1' }, 'slow');
+
+  // })
+
+  // setInterval(function() {
+  //   $('.display-10').delay(1000).animate({ 'fontSize': '12px' }), 1000, 'swing',
+  //     function() {
+  //       $('.display-10').delay(1000).animate({ 'fontSize': '24px' }), 1000;
+  //     };
+  // }, 5000);
+
+
   var scrollButton = document.getElementById('scroll-button');
 
   function showScrollButton() {
@@ -18,16 +38,4 @@ $(document).ready(function() {
     document.documentElement.scrollTop = 0;
   })
 
-
-  $('#accordion').accordion();
-
-  $('.ui-accordion-header').css('background', 'rgba(32, 32, 172, 1)');
-
-
-
-  // $('.display-10').effect('linear', 'slow')
-
-  // $( "#toggle" ).effect( "size", {
-  //   to: { width: 200, height: 60 }
-  // }, 1000 );
 });
